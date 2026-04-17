f60a37ac8b86fb6f3a434a9b72a4b20bd0b4ffe7
YayaSOW <yaya.sow@ism.edu.sn>
2026-04-18 00:37:29 +0200

    nrune

---
+++ nrune.go
@@ -0,0 +1,18 @@
+package student
+
+func NRune(s string, n int) rune {
+	if n <= 0 {
+		return 0
+	}
+
+	count := 0
+
+	for _, r := range s {
+		count++
+		if count == n {
+			return r
+		}
+	}
+
+	return 0
+}
